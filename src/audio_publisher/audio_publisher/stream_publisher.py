46254677c0b745e8df8237da1abfb2cb28b9b9e9
duembgen <frederike.duembgen@epfl.ch>
2020-08-20 16:00:12 +0200

    Create audio publisher

---
+++ src/audio_publisher/audio_publisher/stream_publisher.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+stream_publisher.py: 
+"""
+
+import sys
+import time
+
+import numpy as np
+from scipy.io.wavfile import read, write
+
+import rclpy
+from rclpy.node import Node
+from rcl_interfaces.msg import SetParametersResult
+
+import sounddevice as sd
+
+from audio_interfaces.msg import Signals
+from .publisher import AudioPublisher
+
+N_MICS = 4
+
+class StreamPublisher(AudioPublisher):
+    def __init__(self, Fs, n_buffer=256, publish_rate=None, blocking=False):
+        super().__init__('stream_publisher', n_buffer=n_buffer, publish_rate=publish_rate, Fs=Fs)
+
+        self.duration_ms = 100 * 1000
+        self.n_mics = N_MICS
+
+        sd.default.device = 'default'
+        sd.check_input_settings(sd.default.device, channels=self.n_mics, samplerate=self.Fs)
+
+        mic_positions = np.zeros((N_MICS, 2))
+        self.publish_mic_positions(mic_positions)
+
+        # blocking stream, more ROS-like, better for plotting. However might result in some lost samples 
+        if blocking:
+            self.stream = sd.InputStream(channels=self.n_mics, blocksize=n_buffer)
+            self.stream.start()
+            self.create_timer(1./self.publish_rate, self.publish_loop)
+
+        # non-blocking stream, less ROS-like, problematic for plotting. But we do not lose samples. 
+        else:
+            with sd.InputStream(channels=self.n_mics, callback=self.publish_stream, blocksize=n_buffer) as stream:
+                sd.sleep(self.duration_ms)
+                # need below return or we will stay in this context forever
+                return
+
+    def publish_stream(self, signals_T, frames, time_stream, status): 
+        self.get_logger().debug(f'buffer start time: {time_stream.inputBufferAdcTime}')
+        self.get_logger().debug(f'currentTime: {time_stream.currentTime}')
+
+        if status:
+            print(status)
+
+        self.process_signals(signals_T.T)
+
+    def publish_loop(self): 
+        n_buffer = self.n_buffer
+
+        n_available = self.stream.read_available
+        if n_buffer > n_available:
+            self.get_logger().warn(f'Requesting more frames ({n_buffer}) than available ({n_available})')
+        signals_T, overflow = self.stream.read(n_buffer) # frames x channels
+        if overflow:
+            self.get_logger().warn('overflow')
+
+        self.process_signals(signals_T.T)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    Fs = 44100
+    n_buffer = 2**10
+    publish_rate = int(Fs/n_buffer) #11 # in Hz
+    blocking = False
+
+    print(f'Publishing audio data from stream at {publish_rate}Hz.')
+
+    publisher = StreamPublisher(Fs=Fs, publish_rate=publish_rate, n_buffer=n_buffer, blocking=blocking)
+
+    rclpy.shutdown()
--- src/audio_publisher/setup.py
+++ src/audio_publisher/setup.py
@@ -1,30 +1,27 @@
-from glob import glob
-import os
 from setuptools import setup
 
-package_name = 'audio_stack'
+package_name = 'audio_publisher'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        (os.path.join('share', package_name), ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='duembgen',
     maintainer_email='frederike.duembgen@epfl.ch',
-    description='Processing and plotting of live audio data',
-    license='Apache License 2.0',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'correlator = audio_stack.correlator:main',
-            'processor = audio_stack.processor:main',
+            'file = audio_publisher.file_publisher:main',
+            'stream = audio_publisher.stream_publisher:main'
         ],
     },
 )
