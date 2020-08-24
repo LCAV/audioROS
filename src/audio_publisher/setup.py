e0f99dd71e835d6382c9109c8998f71a73ab1cf6
duembgen <frederike.duembgen@epfl.ch>
2020-08-24 12:26:25 +0200

    Fill description and license information

--- src/audio_publisher/setup.py
+++ src/audio_publisher/setup.py
@@ -15,8 +15,8 @@ setup(
     zip_safe=True,
     maintainer='duembgen',
     maintainer_email='frederike.duembgen@epfl.ch',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Publishing of audio data from different sources',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
