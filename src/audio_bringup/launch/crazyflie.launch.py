d1f5bc5b4b8563679c2d884493e9037aed2f0ade
duembgen <frederike.duembgen@epfl.ch>
2020-08-24 14:15:20 +0200

    Create new launch file.

---
+++ src/audio_bringup/launch/crazyflie.launch.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+crazyflie.launch.py:  Launch the processing pipeline from the Crazyflie CRTP stream.
+"""
+
+import sys
+
+import launch
+
+from audio_bringup.helpers import get_launch_description 
+
+LOG_LEVEL = 'warn'
+
+node_config = {
+    'gateway': {
+        'params': [],
+        'pkg': 'crazyflie_crtp'
+    },
+    'processor': {
+        'params': [{'bf_method':'mvdr'}],
+        'pkg': 'audio_stack'
+    },
+    'correlator': {
+        'params': [{'noise':'', 'frequency':'uniform', 'window':'tukey'}],
+        'pkg': 'audio_stack'
+    }
+}
+
+def generate_launch_description():
+    return get_launch_description(node_config, log_level=LOG_LEVEL)
+
+def main(argv=sys.argv[1:]):
+    ld = generate_launch_description(node_config, log_level=LOG_LEVEL)
+    ls = launch.LaunchService(argv=argv)
+    ls.include_launch_description(ld)
+    return ls.run()
+
+if __name__ == '__main__':
+    main()
