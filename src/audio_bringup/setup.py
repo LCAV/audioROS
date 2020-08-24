ec629077fec269e5f7be9feae4a2d8f2ff345e65
duembgen <frederike.duembgen@epfl.ch>
2020-08-24 12:15:39 +0200

    Create bringup package

--- src/audio_bringup/setup.py
+++ src/audio_bringup/setup.py
@@ -1,28 +1,28 @@
 import os
+from glob import glob
 from setuptools import setup
 
-package_name = 'audio_stack'
+package_name = 'audio_bringup'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             [os.path.join('resource', package_name)]),
         (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='duembgen',
     maintainer_email='frederike.duembgen@epfl.ch',
-    description='Processing and plotting of live audio data',
+    description='Launch files for audio processing',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'correlator = audio_stack.correlator:main',
-            'processor = audio_stack.processor:main',
         ],
     },
 )
