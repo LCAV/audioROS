17d6d7864400c117d0ff4f23a272e884e8e1ab97
duembgen <frederike.duembgen@epfl.ch>
2020-08-25 15:44:24 +0200

    Correct small errors

--- src/audio_stack/audio_stack/doa_estimator.py
+++ src/audio_stack/audio_stack/doa_estimator.py
@@ -49,7 +49,7 @@ class DoaEstimator(Node):
         self.combination_method = "sum"
         parameters = [
                 rclpy.parameter.Parameter("combination_method", rclpy.Parameter.Type.STRING, self.combination_method),
-                rclpy.parameter.Parameter("combination_n", rclpy.Parameter.Type.INT, self.combination_n),
+                rclpy.parameter.Parameter("combination_n", rclpy.Parameter.Type.INTEGER, self.combination_n),
         ]
         self.set_parameters_callback(self.set_params)
         self.set_parameters(parameters)
@@ -60,7 +60,7 @@ class DoaEstimator(Node):
             if param.name == "combination_method":
                 self.combination_method = param.get_parameter_value().string_value
             if param.name == "combination_n":
-                self.combination_n = param.get_parameter_value().int_value
+                self.combination_n = param.get_parameter_value().integer_value
             else:
                 return SetParametersResult(successful=False)
         return SetParametersResult(successful=True)
@@ -73,12 +73,12 @@ class DoaEstimator(Node):
         self.spectrum_orientation_list.append((spectrum, msg_spec.orientation))
 
         # remove outdated spectra
-        while len(self.spectrum_list) >= self.combination_n:
+        while len(self.spectrum_orientation_list) >= self.combination_n:
             self.spectrum_orientation_list.pop(0)
 
         # the combined spectrum is going to be in the coordinate frame of the latest 
         # spectrum.
-        spectra_shifted = self.spectrum_orientation_list[-1:][0] # latest element.
+        spectra_shifted = [self.spectrum_orientation_list[-1][0]] # latest element.
         o_ref = self.spectrum_orientation_list[-1][1] 
         for spectrum, orientation in self.spectrum_orientation_list[:-1]:
             # TODO(FD) do interpolation rather than nearest neighbor.
@@ -93,20 +93,19 @@ class DoaEstimator(Node):
             combined_spectrum = np.product(spectra_shifted, axis=0) # n_frequencies x n_angles
 
         # publish
-        msg_new = Spectrum()
-        msg_new.timestamp = msg_spec.timestamp
-        msg_new.n_frequencies = n_frequencies
-        msg_new.n_angles = combined_spectrum.shape[1]
-        msg_new.frequencies = msg_spec.frequencies
-        msg_new.spectrum_vect = list(combined_spectrum.flatten())
+        msg_new = msg_spec
+        msg_new.spectrum_vect = list(combined_spectrum.astype(float).flatten())
         self.publisher_spectrum.publish(msg_new)
         self.get_logger().info(f'Published spectrum.')
 
         # plot
+        # TODO: read angles vector from topic? 
+        theta_scan = np.linspace(0, 360, msg_new.n_angles)
+        frequencies = np.array(msg_new.frequencies)
         assert len(frequencies) == combined_spectrum.shape[0]
         mask = (frequencies <= MAX_FREQ) & (frequencies >= MIN_FREQ)
         labels=[f"f={f:.0f}Hz" for f in frequencies[mask]]
-        self.plotter.update_lines(spectrum[mask], self.beam_former.theta_scan, labels=labels)
+        self.plotter.update_lines(combined_spectrum[mask], theta_scan, labels=labels)
 
 def main(args=None):
     import os
