d2b322a09a25fac804f17844c5e8e721fdddba77
emilia-szymanska <emiliaszym@gmail.com>
2020-08-26 20:26:34 +0200

    Initialiize pyroom with a function

--- src/audio_simulation/audio_simulation/simulation.py
+++ src/audio_simulation/audio_simulation/simulation.py
@@ -21,7 +21,8 @@ N_MICS = 4
 MIC_DISTANCE = 0.108                                                            # distance between microphones
 ROOM_DIM = [2, 2, 2]                                                            # room dimensions
 SOURCE_POS = [1, 1, 1]                                                          # source position
-
+#WAV_PATH = "./audio_source/white_noise.wav"                                     # source .wav file
+WAV_PATH = "/home/emilia/audioROS/src/audio_simulation/audio_simulation/audio_source/white_noise.wav"
 
 class AudioSimulation(Node):
 
@@ -29,10 +30,7 @@ class AudioSimulation(Node):
         super().__init__('audio_simulation')
         
         # creating the room with the audio source
-        room = pra.ShoeBox(ROOM_DIM)
-        #wav_path = "C:/Users/emilia.szymanska/Downloads/pyroomacoustics-master/examples/samples/guitar_16k.wav"
-        #fs, audio = wavfile.read(wav_path)
-        #room.add_source(SOURCE_POS, signal=audio)
+        room = SetRoom(ROOM_DIM, WAV_PATH)
 
         self.publisher_signals = self.create_publisher(String, 'OK_NOK', 10)
         self.subscription_position = self.create_subscription(Pose, 'crazyflie_position', self.listener_callback, 10)
@@ -45,25 +43,6 @@ class AudioSimulation(Node):
         print(ROTATION)
         print(DRONE_CENTER)
         
-        
-        #rot = R.from_quat(ROTATION)
-        #DRONE_CENTER_REV = list(map(neg, DRONE_CENTER))
-        #MIC_LT = list( map(add, DRONE_CENTER, [-MIC_DISTANCE/2, MIC_DISTANCE/2, 0]))    # left top
-        #MIC_RT = list( map(add, DRONE_CENTER, [MIC_DISTANCE/2, MIC_DISTANCE/2, 0]))     # right top
-        #MIC_RB = list( map(add, DRONE_CENTER, [MIC_DISTANCE/2, -MIC_DISTANCE/2, 0]))    # right bottom
-        #MIC_LB = list( map(add, DRONE_CENTER, [-MIC_DISTANCE/2, -MIC_DISTANCE/2, 0]))   # left bottom
-        #mic_locs = [MIC_LT, MIC_RT, MIC_RB, MIC_LB]
-        
-        # MIC_new_position = (MIC_vector - DRONE_CENTER_vector) * rotation + DRONE_CENTER_vector
-
-        #for i in range(len(mic_locs)):
-        #    tmp = list( map( add, mic_locs[i], DRONE_CENTER_REV ) )         # tmp = (MIC_vector - DRONE_CENTER_vector)
-        #    mic_locs[i] = list( map(add, rot.apply(tmp), DRONE_CENTER))     # (tmp * rotation) + DRONE_CENTER_vector
-
-        #mic_arr = np.c_[    mic_locs[0], mic_locs[1], mic_locs[2], mic_locs[3],    ]
-
-        #room.add_microphone_array(mic_arr)
-        #room.simulate()
 
         # plot the room
         #room.plot(img_order=0)
@@ -74,6 +53,32 @@ class AudioSimulation(Node):
         #msg.data = 'Something was sent'
         #self.publisher_signals.publish(msg)
         #self.get_logger().info('Received data')
+
+
+def SetRoom(room_dim, wav_path):
+    pyroom = pra.ShoeBox(room_dim)
+    fs, audio_source = wavfile.read(wav_path)
+    pyroom.add_source(SOURCE_POS, signal = audio_source)
+    return pyroom
+
+#def Simulation(rotation, pose, pyroom):
+    #rot = R.from_quat(ROTATION)
+    #DRONE_CENTER_REV = list(map(neg, DRONE_CENTER))
+    #MIC_LT = list( map(add, DRONE_CENTER, [-MIC_DISTANCE/2, MIC_DISTANCE/2, 0]))    # left top
+    #MIC_RT = list( map(add, DRONE_CENTER, [MIC_DISTANCE/2, MIC_DISTANCE/2, 0]))     # right top
+    #MIC_RB = list( map(add, DRONE_CENTER, [MIC_DISTANCE/2, -MIC_DISTANCE/2, 0]))    # right bottom
+    #MIC_LB = list( map(add, DRONE_CENTER, [-MIC_DISTANCE/2, -MIC_DISTANCE/2, 0]))   # left bottom
+    #mic_locs = [MIC_LT, MIC_RT, MIC_RB, MIC_LB]
+     
+    # MIC_new_position = (MIC_vector - DRONE_CENTER_vector) * rotation + DRONE_CENTER_vector
+    #for i in range(len(mic_locs)):
+    #    tmp = list( map( add, mic_locs[i], DRONE_CENTER_REV ) )         # tmp = (MIC_vector - DRONE_CENTER_vector)
+    #    mic_locs[i] = list( map(add, rot.apply(tmp), DRONE_CENTER))     # (tmp * rotation) + DRONE_CENTER_vector
+
+    #mic_arr = np.c_[    mic_locs[0], mic_locs[1], mic_locs[2], mic_locs[3],    ]
+
+    #room.add_microphone_array(mic_arr)
+    #room.simulate()
 
 
 
@@ -93,8 +98,6 @@ def main(args=None):
 
 
 
-
-
 if __name__ == '__main__':
     main()        
 
@@ -109,3 +112,9 @@ if __name__ == '__main__':
 #        float64 y
 #        float64 z
 #        float64 w
+
+        #room = pra.ShoeBox(ROOM_DIM)
+        #wav_path = "./audio_source/white_noise.wav"
+        #fs, audio_source = wavfile.read(WAV_PATH)
+        #room.add_source(SOURCE_POS, signal = audio_source)
+
