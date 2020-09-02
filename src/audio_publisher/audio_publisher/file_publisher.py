f09b0e085eef077041b5bc12f5be7233b62f349c
duembgen <frederike.duembgen@epfl.ch>
2020-09-02 17:45:23 +0200

    Enable more file reading

--- src/audio_publisher/audio_publisher/file_publisher.py
+++ src/audio_publisher/audio_publisher/file_publisher.py
@@ -19,37 +19,40 @@ import file_parser as fp
 
 class FilePublisher(AudioPublisher):
     def __init__(
-        self, file_source, loop=False, n_buffer=256, publish_rate=None, mic_fname=""
+        self, file_source, Fs, loop=False, n_buffer=256, publish_rate=None, mic_positions=None
     ):
         """
         :param publish_rate: in Hz, at which rate to publish
         """
-        mic_positions = None
-        if mic_fname != "":
-            mic_positions = np.load(mic_fname)
         super().__init__(
             "file_publisher",
             mic_positions=mic_positions,
             n_buffer=n_buffer,
             publish_rate=publish_rate,
-            Fs=None,
+            Fs=Fs
         )
-
         self.loop = loop
+        self.read_file_source(file_source)
         self.file_idx = 0
-
-        if file_source == "analytical_source":
-            self.signals_full, Fs = fp.read_simulation(file_source)
-        elif file_source == "recordings_16_7_20":
-            signals_props, signals_source, signals_all = fp.read_recordings_16_7_20("high", 0, "white_noise")
-            Fs = fp.parameters[file_source]["Fs"]
-            start_idx = fp.parameters[file_source]["time_index"]
-            self.signals_full = signals_all[:, start_idx:]
-
-        self.set_Fs(Fs)
-        self.len = self.signals_full.shape[1]
-
         self.create_timer(1.0 / self.publish_rate, self.publish_loop)
+
+    def read_file_source(self, file_source):
+        if file_source in ["analytical", "pyroomacoustics"]:
+            signals_props, signals_source, signals_all = fp.read_simulation(file_source)
+        elif file_source == "recordings_16_7_20":
+            signals_props, signals_source, signals_all = fp.read_recordings_16_7_20(
+                    loudness="high", 
+                    gt_degrees=0, 
+                    source="white_noise")
+        elif file_source == "recordings_14_7_20":
+            signals_props, signals_source, signals_all = fp.read_recordings_14_7_20(gt_degrees=0)
+        elif file_source == "recordings_9_7_20":
+            signals_props, signals_source, signals_all = fp.read_recordings_9_7_20(gt_degrees=0)
+
+        signals_full = signals_all
+        start_idx = fp.parameters[file_source]["time_index"]
+        self.signals_full = signals_full[:, start_idx:]
+        self.len = self.signals_full.shape[1]
 
     def publish_loop(self):
         n_buffer = self.n_buffer
@@ -67,27 +70,29 @@ class FilePublisher(AudioPublisher):
 
 
 def main(args=None):
-    import os
-
     rclpy.init(args=args)
 
-    Fs = 44100
     n_buffer = 2 ** 10
+
+    file_source = "analytical"
+    #file_source = "pyroomacoustics"
+    #file_source = "recordings_9_7_20"
+    #file_source = "recordings_14_7_20"
+    #file_source = "recordings_16_7_20"
+
+    Fs = fp.parameters[file_source]["Fs"]
+    mic_positions = fp.parameters[file_source]["mic_positions"]
+
     publish_rate = int(Fs / n_buffer)  # 11 # in Hz
-
     print(f"Publishing audio data from file at {publish_rate}Hz.")
-
-    mic_fname = os.path.abspath(current_dir + "/../../../crazyflie-audio/data/simulated/analytical_mics.npy")
-
-    file_source = "analytical_source"
-    file_source = "recordings_16_7_20"
         
     publisher = FilePublisher(
         file_source,
+        Fs=Fs,
         n_buffer=n_buffer,
         publish_rate=publish_rate,
         loop=True,
-        mic_fname=mic_fname,
+        mic_positions=mic_positions,
     )
     rclpy.spin(publisher)
 
