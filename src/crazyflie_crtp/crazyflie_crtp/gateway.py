8ba845dc5236e5d885da0a733e559505d82b6eb9
duembgen <frederike.duembgen@epfl.ch>
2020-09-03 08:39:03 +0200

    Enable parameter setting

--- src/crazyflie_crtp/crazyflie_crtp/gateway.py
+++ src/crazyflie_crtp/crazyflie_crtp/gateway.py
@@ -8,17 +8,19 @@ sys.path.append(current_dir + "/../../../crazyflie-audio/python/")
 
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import SetParametersResult
 from geometry_msgs.msg import Pose, Point, Quaternion
 
 import numpy as np
 
 from audio_interfaces.msg import SignalsFreq, PoseRaw
 from audio_stack.live_plotter import LivePlotter
-
 from reader_crtp import ReaderCRTP
 
 logging.basicConfig(level=logging.ERROR)
-id = "radio://0/80/2M"
+#id = "radio://0/80/2M"
+#id = "radio://0/48/2M"
+id = "radio://0/69/2M"
 
 MAX_YLIM = 1e13  # set to inf for no effect.
 MIN_YLIM = 1e-13  # set to -inf for no effect.
@@ -26,6 +28,19 @@ MIN_YLIM = 1e-13  # set to -inf for no effect.
 N_MICS = 4
 FS = 32000
 N = 1024
+
+# Crazyflie audio parameters that can be set from here.
+PARAMETERS_TUPLES = [
+    ("debug", rclpy.Parameter.Type.INTEGER, 0),
+    ("send_audio_enable", rclpy.Parameter.Type.INTEGER, 0),
+    ("filter_snr_enable", rclpy.Parameter.Type.INTEGER, 0),
+    ("filter_prop_enable", rclpy.Parameter.Type.INTEGER, 0),
+    ("max_freq", rclpy.Parameter.Type.INTEGER, 100),
+    ("min_freq", rclpy.Parameter.Type.INTEGER, 3000),
+    ("use_iir", rclpy.Parameter.Type.INTEGER, 0),
+    ("ma_window", rclpy.Parameter.Type.INTEGER, 0),
+    ("alpha_iir", rclpy.Parameter.Type.DOUBLE, 0.5)
+]
 
 
 class Gateway(Node):
@@ -54,6 +69,14 @@ class Gateway(Node):
 
         self.reader_crtp = reader_crtp
 
+        parameters = []
+        for param in PARAMETERS_TUPLES:
+            self.declare_parameter(param[0])
+            param_rclpy = rclpy.parameter.Parameter(*param)
+            parameters.append(param_rclpy)
+        self.set_parameters_callback(self.set_audio_params)
+        self.set_parameters(parameters)
+
         # choose high publish rate so that we introduce as little
         # waiting time as possible
         self.create_timer(0.001, self.publish_current_data)
@@ -69,12 +92,20 @@ class Gateway(Node):
 
     def publish_audio_dict(self):
         self.get_logger().info(f"Publishing audio signals.")
+
+        # read audio
         signals_f_vect = self.reader_crtp.audio_dict["data"]
+        if signals_f_vect is None:
+            self.get_logger().warn("Empty audio. Not publishing")
+            return
 
         # read frequencies
         if self.reader_crtp.fbins_dict["published"]:
             self.get_logger().error("Synchronization issue: already published fbins")
         fbins = self.reader_crtp.fbins_dict["data"]
+        if fbins is None:
+            self.get_logger().warn("Empty fbins. Not publishing")
+            return
 
         #This seems to be ok now
         #self.get_logger().info(f"Read fbins: {fbins}")
@@ -148,6 +179,29 @@ class Gateway(Node):
 
         self.prev_position_x = msg_pose.position.x
         self.prev_position_y = msg_pose.position.y
+
+    def set_audio_params(self, params):
+        for param in params:
+            param_tuples = [p for p in PARAMETERS_TUPLES if p[0] == param.name]
+            assert len(param_tuples) == 1
+            if param_tuples[0][1] == rclpy.Parameter.Type.INTEGER:
+                old_value = (
+                    self.get_parameter(param.name).get_parameter_value().integer_value
+                )
+                new_value = param.get_parameter_value().integer_value
+            elif param_tuples[0][1] == rclpy.Parameter.Type.DOUBLE:
+                old_value = (
+                    self.get_parameter(param.name).get_parameter_value().double_value
+                )
+                new_value = param.get_parameter_value().double_value
+            else:
+                raise ValueError(param_tuple)
+
+            self.reader_crtp.cf.param.set_value(f"audio.{param.name}", new_value)
+            self.get_logger().info(
+                f"changing {param.name} from {old_value} to {new_value}"
+            )
+        return SetParametersResult(successful=True)
 
 
 def main(args=None):
