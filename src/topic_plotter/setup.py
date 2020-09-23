67cee3e10dfaf9de6089327331fb723fff0ded11
duembgen <frederike.duembgen@epfl.ch>
2020-09-23 13:39:29 +0200

    Automate process

--- src/topic_plotter/setup.py
+++ src/topic_plotter/setup.py
@@ -4,7 +4,7 @@ package_name = 'topic_plotter'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
