7d5392f0b7e4bf02341c93ce6caa66cc3ab62c93
duembgen <frederike.duembgen@epfl.ch>
2020-10-13 11:17:12 +0200

    Create automated pipeline

--- src/topic_writer/topic_writer/csv_writer.py
+++ src/topic_writer/topic_writer/csv_writer.py
@@ -12,13 +12,10 @@ from audio_interfaces.msg import SignalsFreq, PoseRaw
 
 # Time after which we are sure to have read the full bagfile. Set to something very high for no effect.
 # Used to automate the process of bag file conversion (CTRL+C does not work for some reason)
-TIMEOUT_S = 45 # seconds
-
-FILENAME = "test"
-DIRNAME = "csv_files/"
+TIMEOUT_S = 60*60*2 # seconds
 
 class CsvWriter(Node):
-    def __init__(self, filename=FILENAME, dirname=DIRNAME):
+    def __init__(self):
         super().__init__("csv_writer")
 
         self.subscription_signals_f = self.create_subscription(
@@ -32,17 +29,17 @@ class CsvWriter(Node):
         self.declare_parameter("filename")
         self.declare_parameter("dirname")
         self.set_parameters_callback(self.set_params)
-        parameter1 = rclpy.parameter.Parameter("filename", rclpy.Parameter.Type.STRING, filename)
-        parameter2 = rclpy.parameter.Parameter("dirname", rclpy.Parameter.Type.STRING, dirname)
-        self.set_parameters([parameter1, parameter2])
         
+        self.reset()
+        self.get_logger().info('Subscribed to audio/signals_f and geometry/pose_raw.')
+
+    def reset(self):
         self.header = {"index", "topic"}
         self.rows = []
-        self.index = 0
 
     def listener_callback_signals_f(self, msg):
         row_dict = {
-          "index": self.index,
+          "index": len(self.rows),
           "topic": "audio/signals_f",
           # TODO(FD): can we read these fields automatically?  
           "timestamp": msg.timestamp,
@@ -56,11 +53,10 @@ class CsvWriter(Node):
 
         self.header = set(row_dict.keys()).union(self.header)
         self.rows.append(row_dict)
-        self.index += 1
 
     def listener_callback_pose_raw(self, msg):
         row_dict = {
-          "index": self.index,
+          "index": len(self.rows),
           "topic": "geometry/pose_raw",
           # TODO(FD): can we read these fields automatically?  
           "timestamp": msg.timestamp,
@@ -72,40 +68,46 @@ class CsvWriter(Node):
         }
         self.header = set(row_dict.keys()).union(self.header)
         self.rows.append(row_dict)
-        self.index += 1
 
     def set_params(self, params):
-        dirname = self.get_parameter("dirname").get_parameter_value().string_value
-        filename = self.get_parameter("filename").get_parameter_value().string_value
+        """ Set the parameter. If filename is set, we save the current rows and reset. """
         for param in params:
             if param.name == "dirname":
-                dirname = param.get_parameter_value().string_value
+                return SetParametersResult(successful=True)
+
             elif param.name == "filename":
                 filename = param.get_parameter_value().string_value
+                dirname = self.get_parameter("dirname").get_parameter_value().string_value
 
         if filename[-4:] != '.csv':
             filename = filename + '.csv'
 
-        self.fullname = os.path.join(dirname, filename)
-        #self.get_logger().debug(f"will write {self.fullname} on KeyboardInterrupt.") 
+        fullname = os.path.join(dirname, filename)
+        self.write_file(fullname)
         return SetParametersResult(successful=True)
 
-    def write_file(self):
+    def write_file(self, fullname):
         if not self.rows:
-            self.get_logger().info("empty rows, not saving.")
+            self.get_logger().info("Empty rows, not saving.")
             return
 
-        with open(self.fullname, "w+") as f:
+        if not os.path.exists(fullname):
+            with open(fullname, "w+") as f:
+                csv_writer = csv.DictWriter(f, sorted(self.header))
+                csv_writer.writeheader()
+            print(f"Wrote header in new file {fullname}.")
+
+        with open(fullname, "a") as f:
             csv_writer = csv.DictWriter(f, sorted(self.header))
-            csv_writer.writeheader()
             csv_writer.writerows(self.rows)
 
-        self.get_logger().info(f"Wrote {self.fullname}.")
+        self.get_logger().info(f"Appended {len(self.rows)} rows to {fullname}.")
+        self.reset()
 
-    #def destroy_node(self):
-    #    print("custom destroy")
-    #    self.write_file()
-    #    super().destroy_node()
+#    def destroy_node(self):
+#        self.get_logger().info("Custom destroy")
+#        self.write_file()
+#        super().destroy_node()
 
 
 def main(args=None):
@@ -118,14 +120,12 @@ def main(args=None):
         start_time = time.time()
         while (time.time() - start_time) < TIMEOUT_S:
             rclpy.spin_once(writer)
-        writer.get_logger().info("timeout occured. writing...")
-        writer.write_file()
+        writer.get_logger().info("Timeout occured...")
     except KeyboardInterrupt:
-        writer.get_logger().info("KeyboardInterrupt occured. writing...")
-        writer.write_file()
+        writer.get_logger().info("KeyboardInterrupt occured...")
 
     writer.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
