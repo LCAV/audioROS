7a368d08c1983023e65b68fe28e0d9c459f2027f
duembgen <frederike.duembgen@epfl.ch>
2020-11-02 11:59:25 +0100

    Split topic_plotter.audio in two and complete launch file

--- src/audio_bringup/launch/file.launch.py
+++ src/audio_bringup/launch/file.launch.py
@@ -15,14 +15,18 @@ LOG_LEVEL = "warn"
 
 node_config = {
     "file": {
-        "params": [{"n_buffer": 2 ** 8, "publish_rate": 100}],
+        "params": [{"n_buffer": 2048, "publish_rate": 100}],
         "pkg": "audio_publisher",
     },
-    "spectrum_estimator": {"params": [{"bf_method": "mvdr"}], "pkg": "audio_stack"},
     "processor": {
-        "params": [{"noise": "", "frequency": "uniform", "window": "tukey"}],
+        "params": [{"noise": "", "frequency": "uniform", "window": ""}],
         "pkg": "audio_stack",
     },
+    "spectrum_estimator": {"params": [{"bf_method": "mvdr"}], "pkg": "audio_stack"},
+    "doa_estimator": {"pkg": "audio_stack"},
+    "audio": {"pkg": "topic_plotter"},
+    "doa": {"pkg": "topic_plotter"},
+    "geometry": {"pkg": "topic_plotter"},
 }
 
 
