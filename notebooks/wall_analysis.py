ae8100d05f73851634dd9309e00ae4269dbf63b8
duembgen <frederike.duembgen@epfl.ch>
2020-12-11 12:59:29 +0100

    Add appendix

--- notebooks/wall_analysis.py
+++ notebooks/wall_analysis.py
@@ -125,7 +125,7 @@ def parse_calibration_experiments():
         '2020_12_11_calibration',
     ]
     source_list = ['sweep', 'sweep_buzzer']
-    appendix_list = ['']
+    appendix_list = ['', '_BC329']
     filter_list = [0, 1] # snr and props
     motors_list = [0, 'all43000']
 
