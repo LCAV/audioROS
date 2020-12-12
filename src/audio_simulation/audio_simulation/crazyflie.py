9a5312daf9ee2708aaaca7a83c7f0605e9be5c69
emilia-szymanska <emiliaszym@gmail.com>
2020-12-12 22:03:09 +0100

    Add PoseRaw creation, timer with publisher, self.signals and LOOP flag

--- src/audio_simulation/audio_simulation/crazyflie.py
+++ src/audio_simulation/audio_simulation/crazyflie.py
@@ -9,14 +9,14 @@ import sys
 import numpy as np
 import pyroomacoustics as pra
 from scipy.spatial.transform import Rotation as R
-from scipy.io import wavfile
+from math import atan2, degrees
 
 import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Pose
 
-from audio_interfaces.msg import Signals
+from audio_interfaces.msg import Signals, PoseRaw
 from audio_interfaces_py.messages import create_signals_message
 from crazyflie_description_py.parameters import MIC_POSITIONS, FS, N_BUFFER, N_MICS, HEIGHT_MIC_ARRAY
 
@@ -28,6 +28,7 @@ ROOM_DIM = [2, 2, 2]  # room dimensions
 SOURCE_POS = [1, 1, 1]  # source position
 MAX_TIMESTAMP = 2**32 - 1  # max value of uint32
 NUM_REFLECTIONS = 5 # number of reflections to consider in pyroomacoustis.
+LOOP = False # flag for looping the signal after reaching the end
 
 
 class AudioSimulation(Node):
@@ -37,17 +38,73 @@ class AudioSimulation(Node):
         self.room = set_room(ROOM_DIM, N_BUFFER, [SOURCE_POS,], FS)
         self.time_idx = 0
 
+        self.publisher_rawpose = self.create_publisher(PoseRaw, "geometry/pose_raw", 10)
         self.publisher_signals = self.create_publisher(Signals, "audio/signals", 10)
         self.subscription_position = self.create_subscription(
             Pose, "geometry/pose", self.pose_listener_callback, 10
         )
 
+        self.create_timer(N_BUFFER/FS, self.constant_publisher)
+
+        #TODO: there has to be a better way to initialize it
+        self.signals = []
+        self.mic_positions_global = []
+        self.first_sample = 0
+        self.current_pose = Pose()
         self.mic_positions = np.array(MIC_POSITIONS)
+    
+    def constant_publisher(self):
+        """
+        Publish signal chunks at a rate defined by the timer
+        """
+        
+        
+        if len(self.signals) == 0:
+            return 
+        
+        if self.time_idx == 0:
+            self.time_idx = starting_sample_nr(self.room, self.mic_positions_global)
+            self.first_sample = self.time_idx 
+
+        if self.time_idx + N_BUFFER < self.signals.shape[1]:
+            signal_to_send = np.zeros((N_MICS, N_BUFFER), dtype=float)
+        else:
+            signal_to_send = np.zeros((N_MICS, (self.signals.shape[1] - self.first_sample) % N_BUFFER), dtype=float)
+
+        for i in range(N_MICS):
+            signal_to_send[i] = self.signals[i][self.time_idx:self.time_idx+N_BUFFER]
+
+        msg = create_signals_message(signal_to_send, self.mic_positions, self.time_idx, self.room.fs)
+        self.publisher_signals.publish(msg)
+        self.get_logger().info(f"{self.time_idx}: Published audio signal")
+        self.time_idx += N_BUFFER
+       
+        #TODO two ifs => similar issue?
+        # shutdown => exiting the console command
+        if self.time_idx >= MAX_TIMESTAMP:
+            self.get_logger().warn("timestamp overflow")
+            if LOOP:
+                self.time_idx = starting_sample_nr(self.room, self.mic_positions_global)
+                self.first_sample = self.time_idx 
+            else:
+                self.get_logger().warn("exiting the node")
+                self.destroy_node()
+                rclpy.shutdown()
+        
+        if self.time_idx >= self.signals.shape[1]:
+            if LOOP:
+                self.get_logger().warn("end of signal array - starting sending from the beginning")
+                self.time_idx = starting_sample_nr(self.room, self.mic_positions_global)
+                self.first_sample = self.time_idx 
+            else:
+                self.get_logger().warn("end of signal array - exiting the node")
+                self.destroy_node()
+                rclpy.shutdown()
 
 
     def pose_listener_callback(self, msg_pose):
         """
-        Run a simulation and send the audio signal in packets when a message is received 
+        Run a simulation, update a signal content and publish a PoseRaw() message  when a new pose is received 
         """
 
         rotation_rx = msg_pose.orientation
@@ -60,30 +117,15 @@ class AudioSimulation(Node):
             [drone_center_rx.x, drone_center_rx.y, drone_center_rx.z]
         )
 
-        mic_positions_global = global_mic_positions(self.mic_positions, rotation, drone_center)
-        sim_room = self.simulation(mic_positions_global)
+        self.mic_positions_global = global_mic_positions(self.mic_positions, rotation, drone_center)
+        sim_room = self.simulation(self.mic_positions_global)
+        self.signals = sim_room.mic_array.signals
 
-        # TODO(FD): for the moment, we send the first non-zero signal at this position, and 
-        # we do not take into account the timestamp of the pose. In a more realistic simulation, 
-        # we should send the signal that corresponds to the actual current time, given by 
-        # the timestamp of the pose. 
-        start_sample = starting_sample_nr(sim_room, mic_positions_global)
+        pose_raw = create_pose_raw_message(self.current_pose, msg_pose)
+        self.current_pose = msg_pose
 
-        signal_to_send = np.zeros((N_MICS, N_BUFFER), dtype=float)
-
-        for i in range(N_MICS):
-            signal_to_send[i] = sim_room.mic_array.signals[i][start_sample:start_sample+N_BUFFER]
-
-        msg = create_signals_message(signal_to_send, self.mic_positions, self.time_idx, sim_room.fs)
-
-        self.publisher_signals.publish(msg)
-
-        self.get_logger().info(f"{self.time_idx}: Published audio signal")
-        self.time_idx += 1
-
-        if self.time_idx >= MAX_TIMESTAMP:
-            self.get_logger().warn("timestamp overflow")
-            self.time_idx = self.time_idx % MAX_TIMESTAMP
+        self.publisher_rawpose.publish(pose_raw)
+        self.get_logger().info("Published pose raw")
 
 
     def simulation(self, mic_array):
@@ -106,6 +148,44 @@ class AudioSimulation(Node):
                 self.room.sources[i].position, signal=self.room.sources[i].signal
             )
         return pyroom_cp
+
+
+def create_pose_raw_message(previous_pose, current_pose):
+    """
+    Create a message of type PoseRaw() based on previous and current pose
+    """
+    
+    prev_position = previous_pose.position 
+    curr_position = current_pose.position 
+    curr_orientation = current_pose.orientation 
+    
+    pose_raw = PoseRaw()
+    #TODO change timestamp!
+    pose_raw.timestamp = 0 
+    pose_raw.dx = curr_position.x - prev_position.x
+    pose_raw.dy = curr_position.y - prev_position.y
+    pose_raw.z = curr_position.z
+    pose_raw.yaw_deg = get_yaw(curr_orientation)
+    pose_raw.yaw_rate_deg = 0.0
+    #TODO ground truth source direction in [-180, 180] degrees
+    pose_raw.source_direction_deg = 0.0
+
+    return pose_raw
+
+
+def get_yaw(quaternion):
+    """
+    Calculate yaw from a quaternion
+    """
+
+    x = quaternion.x
+    y = quaternion.y
+    z = quaternion.z
+    w = quaternion.z
+
+    yaw = degrees(atan2(2.0 * (y*z + w*x), w*w -  x*x - y*y + z*z))
+    
+    return yaw
 
 
 def starting_sample_nr(pyroom, mic_array):
