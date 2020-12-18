98f13b899c45205df8336c7f215532c5c61c7668
Frederike Duembgen <frederike.duembgen@epfl.ch>
2020-12-18 10:43:40 +0100

    Prepare scripts for flying experiments

--- src/crazyflie_description/crazyflie_description_py/commands.py
+++ src/crazyflie_description/crazyflie_description_py/commands.py
@@ -24,10 +24,9 @@ command_dict = {
      ],
      'linear':[
         ('/gateway', 'hover_height', 0.5, 3),
-        ('/gateway', 'move_distance', 0.5, 2), # 50 cm
+        ('/gateway', 'move_distance', 0.5, 12), # 50 cm
         ('/gateway', 'land_velocity', 0.2, 3),
      ],
-
      'all43000': [
         ('/gateway', 'all', 43000, 1) # 165 is duration of the 50cm move
      ],
