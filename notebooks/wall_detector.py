b1b4dda9b453df890b22f9be9927d4047b123954
duembgen <frederike.duembgen@epfl.ch>
2021-01-27 11:56:44 +0100

    Analyze frequency slices

--- notebooks/wall_detector.py
+++ notebooks/wall_detector.py
@@ -14,6 +14,15 @@ DISTANCE = 0
 FREQUENCY = 0
 N_MICS = 4
 
+def sort_and_clip(array, min_, max_):
+    array = np.sort(array)
+    if max_ is not None:
+        array = array[array < max_]
+    if min_ is not None:
+        array = array[array > min_]
+    return array
+
+
 class WallDetector(object): 
     def __init__(self, params={}, n_mics=N_MICS):
         self.df = pd.DataFrame(columns=['time', 'counter', 'mic', 'frequency', 'distance', 'angle', 'magnitude'])
@@ -35,7 +44,15 @@ class WallDetector(object):
         self.df = pd.concat([self.df, df])
 
 
-    def get_frequency_slice(self, distance):
+    def get_distances(self, min_dist=None, max_dist=None):
+        return sort_and_clip(self.df.distance.unique(), min_dist, max_dist)
+
+
+    def get_frequencies(self, min_freq=None, max_freq=None):
+        return sort_and_clip(self.df.frequency.unique(), min_freq, max_freq)
+
+
+    def get_frequency_slice(self, distance, min_freq=None, max_freq=None):
         """ 
         :return: slice along one distance, of shape (mic x frequencies), frequencies
         """
@@ -43,7 +60,7 @@ class WallDetector(object):
         if not len(df):
             return 
 
-        freqs = self.get_frequencies()
+        freqs = self.get_frequencies(min_freq, max_freq)
         freq_slice = np.empty((self.n_mics, len(freqs)))
         for j, f in enumerate(freqs):
             for i in range(self.n_mics):
@@ -52,22 +69,14 @@ class WallDetector(object):
         return freq_slice, freqs
 
 
-    def get_distances(self):
-        return sorted(self.df.distance.unique())
-
-
-    def get_frequencies(self):
-        return sorted(self.df.frequency.unique())
-
-
-    def get_distance_slice(self, frequency):
+    def get_distance_slice(self, frequency, min_dist=None, max_dist=None):
         """ 
         :return: slice along one distance, of shape (mic x frequencies), frequencies
         """
         df = self.df.loc[self.df.frequency == frequency]
         if not len(df):
             return 
-        distances = sorted(df.distance.unique())
+        distances = self.get_distances(min_dist, max_dist)
 
         distance_slice = np.empty((self.n_mics, len(distances)))
         for j, d in enumerate(distances):
@@ -77,16 +86,20 @@ class WallDetector(object):
         return distance_slice, distances
 
 
-    def get_df_matrix(self):
-        distances = self.get_distances()
-        frequencies = sorted(self.df.frequency.unique())
+    def get_df_matrix(self, max_freq=None, min_freq=None, min_dist=None, max_dist=None):
+        distances = self.get_distances(min_dist, max_dist)
+        frequencies = self.get_frequencies(min_freq, max_freq)
 
         df_matrix = np.zeros((self.n_mics, len(frequencies), len(distances)))
 
         for (d, f, m), df in self.df.groupby(['distance', 'frequency', 'mic']):
-            d_i = np.where(distances == d)[0][0]
-            f_i = np.where(frequencies == f)[0][0]
-            df_matrix[m, f_i, d_i] = np.median(df.magnitude.values)
+            try:
+                d_i = np.where(distances == d)[0][0]
+                f_i = np.where(frequencies == f)[0][0]
+            except:
+                continue
+            median = np.nanmedian(df.magnitude.values)
+            df_matrix[m, f_i, d_i] = median
         return distances, frequencies, df_matrix
 
         for k, d in enumerate(distances):
