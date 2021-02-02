390dab7abf37071bde9001e8cd978dbf382c39af
duembgen <frederike.duembgen@epfl.ch>
2021-02-02 17:24:07 +0100

    Update packages

--- src/topic_writer/setup.py
+++ src/topic_writer/setup.py
@@ -15,13 +15,12 @@ setup(
     zip_safe=True,
     maintainer='duembgen',
     maintainer_email='frederike.duembgen@epfl.ch',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Write data streams from topics to files',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'csv_writer = topic_writer.csv_writer:main',
-            'convert_all = topic_writer.convert_all:main'
         ],
     },
 )
