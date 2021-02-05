723ccf484888f620716c48cdee07ab14a1dc5954
duembgen <frederike.duembgen@epfl.ch>
2021-02-05 15:43:59 +0100

    Improve simulation

--- notebooks/simulation.py
+++ notebooks/simulation.py
@@ -14,6 +14,7 @@ from signals import generate_signal
 DURATION_SEC = 38
 N_TIMES = DURATION_SEC * FS // (N_BUFFER * 2)
 D0 = 0.1 # distance between mic and source for single-mic case, in meters
+ATTENUATION = 20 # attenuation coefficient in dB / m, 10: between 100-200 degrees, 4000+ Hz
 
 # this corresponds to the setup in BC325: 
 Y_OFFSET = 0.08 # in meters
@@ -66,30 +67,34 @@ def get_average_magnitude(room, signal, n_buffer=N_BUFFER, n_times=N_TIMES):
     
     assert (n_times * n_buffer) < room.mic_array.signals.shape[1], f"{n_times}*{n_buffer}={n_times * n_buffer}, {room.mic_array.signals.shape}"
     
-    y_f_list = []
+    h_f_list = []
     idx = 0
-    #input_f = np.fft.rfft(signal[:n_buffer]) # n_frequencies
     for _ in range(n_times):
+        input_f = np.fft.rfft(signal[idx:idx+n_buffer]) 
         output_f = np.fft.rfft(room.mic_array.signals[:, idx:idx+n_buffer], axis=1) # n_mics x n_frequencies
-        #h_f_list.append(output_f[None, ...]/input_f[None, None, :])
-        y_f_list.append(output_f[None, ...] / n_buffer)
+        ratio_f = output_f[None, ...]/input_f[None, None, :]
+
+        # TODO(FD) this ratio is not between 0 and 1. Find out if this is normal. 
+        #print('ratio:', np.min(np.abs(ratio_f)), np.max(np.abs(ratio_f)))
+        h_f_list.append(ratio_f)
         idx += n_buffer
-    y_f = np.concatenate(y_f_list, axis=0) # n_times x n_mics x n_frequencies
-    return np.mean(np.abs(y_f), axis=0) #/ n_buffer
+    h_f = np.concatenate(h_f_list, axis=0) # n_times x n_mics x n_frequencies
+
+    start_idx = n_times // 5 # ignore first few buffers because of border effects.
+    return np.mean(np.abs(h_f[start_idx:]), axis=0) 
 
 
-def get_slice_freqs(mic_positions, source, frequencies, gain=1.0):
+def get_slice_theory(mic_positions, source, frequencies, gain=1.0):
     # simplification1: attenuation normally depends on pressure, humidity, frequency.
     # simplification 2: no extra loss at wall
-    a = 10 # attenuation coefficient in dB / m, between 100-200 degrees, 4000+ Hz
     source_image = [source[0], -source[1]]
     n_mics = mic_positions.shape[0]
     H = np.empty((n_mics, len(frequencies)))
     for i, mic in enumerate(mic_positions):
         direct_path = np.linalg.norm(mic - source)
         reflect_path = np.linalg.norm(mic - source_image)
-        alpha0 = 10**(-a*direct_path/20)
-        alpha1 = 10**(-a*reflect_path/20)
+        alpha0 = 10**(-ATTENUATION*direct_path/20)
+        alpha1 = 10**(-ATTENUATION*reflect_path/20)
         n0 = direct_path / SPEED_OF_SOUND
         n1 = reflect_path / SPEED_OF_SOUND
         H_ij = alpha0 * gain * np.exp(-1j*2*np.pi*frequencies*n0) + \
@@ -97,15 +102,6 @@ def get_slice_freqs(mic_positions, source, frequencies, gain=1.0):
         H[i, :] = np.abs(H_ij)
     return H
 
-
-def get_slice(frequencies, distance_cm=0, yaw_deg=0, single_mic=False, ax=None, gain=1.0):
-    """ 
-    Generate one-wall RIR using analytical formula. 
-
-    Returns list of length n_mics, where each element contains n_frequencies complex values.
-    """
-    source, mic_positions = get_setup(distance_cm, yaw_deg, ax, single_mic)
-    return get_slice_freqs(mic_positions, source, frequencies, gain=gain)
 
 
 def get_df_theory(frequencies, distances, gain=1.0, chosen_mics=range(4)):
@@ -116,7 +112,7 @@ def get_df_theory(frequencies, distances, gain=1.0, chosen_mics=range(4)):
     H = np.zeros((n_mics, len(frequencies), len(distances)))
     for k, distance in enumerate(distances):
         source, mic_positions = get_setup(distance, yaw_deg=0, ax=None, single_mic=False)
-        H[:, :, k] = get_slice_freqs(mic_positions[chosen_mics], source, frequencies, gain=gain)
+        H[:, :, k] = get_slice_theory(mic_positions[chosen_mics], source, frequencies, gain=gain)
     return H
 
 
@@ -142,10 +138,10 @@ def get_freq_slice_pyroom(frequencies, distance_cm, yaw_deg=0, signal=None):
         try:
             signal = pd.read_pickle('results/multi.pk')
         except FileNotFoundError:
-            print('Run WallStudy notebook to save multi.pk')
+            print('Run WallStudy notebook to save results/multi.pk')
 
     n_times = len(signal) // N_BUFFER
-    mag = get_average_magnitude(room, signal, n_buffer=N_BUFFER, n_times=n_times) # n_times x n_mics x n_frequencies
+    mag = get_average_magnitude(room, signal, n_buffer=N_BUFFER, n_times=n_times) # n_mics x n_frequencies
     freqs_all = np.fft.rfftfreq(N_BUFFER, 1/FS)
     if len(frequencies) < len(freqs_all):
         bins_ = [get_bin(freqs_all, f) for f in frequencies]
@@ -155,7 +151,8 @@ def get_freq_slice_pyroom(frequencies, distance_cm, yaw_deg=0, signal=None):
 
 
 def get_freq_slice_theory(frequencies, distance_cm, yaw_deg=0):
-    Hs = get_slice(frequencies, distance_cm, yaw_deg, single_mic=False, ax=None)
+    source, mic_positions = get_setup(distance_cm, yaw_deg, ax=None, single_mic=False)
+    Hs = get_slice_theory(mic_positions, source, frequencies, gain=1.0)
     return [np.abs(H) for H in Hs]
 
 
@@ -175,10 +172,11 @@ def get_dist_slice_pyroom(frequency, distances_cm, yaw_deg=0, n_times=100):
     return np.array(Hs)
 
 
-def get_dist_slice_theory(frequency, distances_cm, yaw_deg=0):
+def get_dist_slice_theory(frequency, distances_cm, yaw_deg=0, gain=1.0):
     Hs = []
     for d in distances_cm:
-        H = get_slice(np.array([frequency]), d, yaw_deg, single_mic=False, ax=None, gain=5)
+        source, mic_positions = get_setup(d, yaw_deg, ax=None, single_mic=False)
+        H = get_slice_theory(mic_positions, source, np.array([frequency]), gain=gain)
         H = np.array(H)
         assert H.shape[1] == 1, H.shape
         Hs.append(np.abs(H[:, 0]))
