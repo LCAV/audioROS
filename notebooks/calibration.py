e190e68cf9ad2631190490d04e1f4f43cd7432de
duembgen <frederike.duembgen@epfl.ch>
2021-03-05 12:12:54 +0100

    Add 'offline' calibration

--- notebooks/calibration.py
+++ notebooks/calibration.py
@@ -9,6 +9,7 @@ import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
 
+
 def plot_calibration(x, ys, function, ax): 
     uniform_x = np.linspace(min(x), max(x), 100)
     uniform_ys = function(uniform_x)
@@ -51,11 +52,12 @@ def get_calibration_function_matrix(df_matrix, df_freq, ax=None):
     from wall_detector import prune_df_matrix
 
     df_matrix_pruned, df_freq, __ = prune_df_matrix(df_matrix, df_freq)
-    median_values = np.median(df_matrix_pruned, axis=2)
+    median_values = np.nanmedian(df_matrix_pruned, axis=2) # n_mics x n_freqs
     calib_function = interp1d(df_freq, median_values, 
                               kind="quadratic", fill_value="extrapolate")
     if ax is not None:
         plot_calibration(df_freq, median_values, calib_function, ax=ax)
+    np.testing.assert_allclose(calib_function(df_freq[0]), median_values[:, 0])
     return calib_function
 
 
