a357023ce77dd931df9d0d52bbc82902a5980a32
duembgen <frederike.duembgen@epfl.ch>
2021-06-17 13:11:18 +0200

    Adjust sweep parameters

--- src/crazyflie_description/crazyflie_description_py/parameters.py
+++ src/crazyflie_description/crazyflie_description_py/parameters.py
@@ -15,18 +15,14 @@ MIC_POSITIONS = [
     [MIC_D * m for m in mics] for mics in MIC_POSITIONS_UNIT
 ]  # relative mic positions (meters)
 
-HEIGHT_MIC_ARRAY = (
-    0.0  # height of mic array with respect to drone center (in meters)
-)
+HEIGHT_MIC_ARRAY = 0.0  # height of mic array with respect to drone center (in meters)
 
 BUZZER_POSITION = [[0.0, 0.0]]  # relative buzzer position (in meters)
-HEIGHT_BUZZER = (
-    0.01  # height of buzzer with resepect to drone center (in meters)
-)
+HEIGHT_BUZZER = 0.01  # height of buzzer with resepect to drone center (in meters)
 
 N_MICS = 4  # number of mics
-FS = 32000  # sampling frequency [Hz]
-N_BUFFER = 2048  # number of samples in audio buffer
+FS = 64000  # sampling frequency [Hz]
+N_BUFFER = 1024  # number of samples in audio buffer
 FFTSIZE = 32  # number of frequency bins that are sent.
 
 # name: (effect_number, [min_freq_Hz, max_freq_Hz], duration_sec)
