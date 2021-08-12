02434d6138d14c455c36bd66bddfd1ed7325ec31
Adrien Hoffet <adrien.hoffet@epfl.ch>
2021-08-12 17:23:55 +0200

    Change constants for a smooth printout

--- python/visualizations/constants.py
+++ python/visualizations/constants.py
@@ -7,7 +7,7 @@ constants.py:
 THRESHOLD = 50  # 100
 ROI_X = [0, 1]  # [0.4, 0.8]  #
 ROI_Y = [0, 1]  # [0.25, 0.75]
-RADIUS = 10  # 2  # in pixels
-CONSECUTIVE_FRAMES = 4
+RADIUS = 135 # 2  # in pixels
+CONSECUTIVE_FRAMES = 50
 N_IMAGES_BACKGROUND = 10
 SAVE_EVERY_K = 100
